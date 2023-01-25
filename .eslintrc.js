e1cde4fdff1b26a6a5cbd9505c9029d59ba7071a
leeyongtae~ <likemind@hanmail.net>
2023-01-25 22:33:46 +0900

    VSCode에서 발생하는 Delete `␍` eslint (prettier/prettier) 해결방법

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,35 +4,37 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "plugin:prettier/recommended",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
   ],
+
   parserOptions: {
-    parser: "@babel/eslint-parser",
+    parser: '@babel/eslint-parser',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "prettier/prettier": [
-      "error",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': [
+      'error',
       {
         singleQuote: true,
         semi: true,
         useTabs: false,
         tabWidth: 2,
-        trailingComma: "all",
+        trailingComma: 'all',
         printWidth: 80,
         bracketSpacing: true,
-        arrowParens: "avoid",
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
       },
     ],
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
         jest: true,
