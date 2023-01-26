e1c8588b5a8246cfb186153ce6d5ae559a25969c
yt6103.lee <likemind.lee@partner.multicampus.com>
2023-01-26 21:37:31 +0900

    초기 라우터 파일

---
+++ src/components/InputTag.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>InputTag......................</div>
+</template>
+
+<script>
+export default {
+  name: 'MelonFrontInputTag',
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Home.....</div>
+</template>
+
+<script>
+export default {
+  name: 'MelonFrontHome',
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
